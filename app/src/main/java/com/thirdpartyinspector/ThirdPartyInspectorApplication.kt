6e170e020f3d44d101ac7b841b748eb8b4eb369d
Tkaay94-cmd <Tkaay.94@hotmail.com>
2026-09-08 10:23:51 +0930

    Create app/src/main/java/com/thirdpartyinspector/ThirdPartyInspectorApplication.kt

---
+++ app/src/main/java/com/thirdpartyinspector/ThirdPartyInspectorApplication.kt
@@ -0,0 +1,13 @@
+kotlin
+package com.thirdpartyinspector
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ThirdPartyInspectorApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize logging, analytics (NONE by default), or other on-start components here.
+    }
+}
