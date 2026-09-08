0afa39d4d5c21b617783d35453f993f461568f1e
Tkaay94-cmd <Tkaay.94@hotmail.com>
2026-09-08 10:28:12 +0930

    Create app/src/main/java/com/thirdpartyinspector/domain/model/FindingCategory.kt

---
+++ app/src/main/java/com/thirdpartyinspector/domain/model/FindingCategory.kt
@@ -0,0 +1,13 @@
+kotlin
+package com.thirdpartyinspector.domain.model
+
+enum class FindingCategory {
+    PACKAGE,
+    PERMISSION,
+    ACCESSIBILITY,
+    DEVICE_ADMIN,
+    FILE,
+    NETWORK,
+    ROOT,
+    OTHER
+}
